99392aa0136d58513ed0b7eec073e355838662bd
Sanjaiprasath <161956373+Sanjaiprasath68@users.noreply.github.com>
2024-07-01 02:58:40 +0530

    update

--- website/src/components/Projects/Projects.js
+++ website/src/components/Projects/Projects.js
@@ -5,7 +5,7 @@ import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import news_Api from "../../Assets/Projects/news_Api.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
@@ -23,12 +23,12 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={news_Api}
               isBlog={false}
-              title="Chatify"
+              title="NewsApi"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              ghLink="https://github.com/Sanjaiprasath68/News_Api"
+              demoLink="https://news-articles-six.vercel.app/"
             />
           </Col>
 
